a49607ab6e8694bc8312e300f65d173d327595c3
Roman Coedo <romancoedo@gmail.com>
2015-06-17 23:56:10 +0200

    new emacs

---
+++ emacs/emacs.d/init.el
@@ -0,0 +1,15 @@
+(require 'cask "/usr/local/share/emacs/site-lisp/cask.el")
+(cask-initialize)
+(require 'pallet)
+(pallet-mode t)
+(add-to-list 'load-path "~/.emacs.d/lisp")
+
+(load "global.el")
+(load "evil-mode.el")
+
+(require 'helm)
+(helm-mode 1)
+(helm-autoresize-mode 1)
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to do persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
---
+++ emacs/emacs.d/lisp/global.el
@@ -0,0 +1,3 @@
+(setq inhibit-splash-screen t)
+(tool-bar-mode -1)
+(osx-clipboard-mode t)
