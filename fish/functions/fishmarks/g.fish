54fdbda7ef425f94d4000715b8e9868ba5252ee6
Roman Coedo <romancoedo@gmail.com>
2015-06-26 17:27:07 +0200

    Fix g.fish

--- fish/functions/fishmarks/g.fish
+++ fish/functions/fishmarks/g.fish
@@ -1,11 +1,10 @@
 function g
     set -l temp /tmp/temp_bookmark.result
-    list_bookmarks |  tr -s " " | awk '{ print $1 }' | peco > $temp
-    set -l bookmark (cat $temp)
+    list_bookmarks |  tr -s " " | awk '{ print $1 }' | peco | perl -pe 's/\x1b.*?[mGKH]//g' > $temp
 
+    set -l bookmark (cat $temp)
     if test (count $bookmark) -gt 0
-        cd (list_bookmarks | grep -F $bookmark | tr -s " " | awk '{ print $3 }')
-        #cd (print_bookmark $bookmark)
+        cd (print_bookmark $bookmark)
     end
 end
 
