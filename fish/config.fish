7d38fddc888199f308ff6b9d6cf66403b7059d44
Roman Coedo <romancoedo@gmail.com>
2015-10-14 17:42:31 +0200

    Add CDPATH

--- fish/config.fish
+++ fish/config.fish
@@ -6,6 +6,7 @@ set fish_function_path $fish_function_path (find $HOME/.config/fish/functions/* 
 set -x EDITOR "vim"
 set -x GIT_EDITOR "vim"
 set -x GREP_OPTIONS "--color=auto"
+set -x CDPATH . "$HOME/Workspace"
 
 source $HOME/.config/fish/env.fish
 source $HOME/.config/fish/colors.fish
