af6a19968e2763e28053115434ced0b0b5be3afd
Roman Coedo <romancoedo@gmail.com>
2015-10-14 17:34:02 +0200

    Add interactive cd function

--- fish/functions/fish/icd.fish
+++ fish/functions/fish/icd.fish
@@ -1,7 +1,8 @@
 function icd
-    ls -d */ | peco | read tempvar
+    tree --noreport -d -i -L 1 $CDPATH | grep -v '\(Users\|\.\)' | peco | read tempvar
     if [ $tempvar ]
         cd $tempvar
+        commandline -f repaint
     end
 end
 
