77e5463ab6ac0bc0051e95ee5933e3798f6c78bc
Roman Coedo <romancoedo@gmail.com>
2016-03-03 10:17:50 +0100

    testing object oriented lua. yes, I'm a noob :(

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -108,6 +108,38 @@ local cmd = {"cmd"}
 local alt = {"alt"}
 local cmdalt = {"cmd", "alt"}
 
+ModalKey = {}
+function ModalKey.new()
+  local mode = nil
+  local modeTooltip = nil
+
+  local function text()
+    return hs.styledtext.ansi(mode, {font={name="Fantasque Sans Mono",size=36}, backgroundColor={alpha=1}})
+  end
+
+  local function hideModeTooltip()
+    if modeTooltip then
+      modeTooltip:delete()
+    end
+  end
+
+  local function showModeTooltip()
+    clearModeTooltip()
+    modeTooltip = hs.drawing.text(hs.geometry.rect(100,100,1000,1000), text())
+    modeTooltip:show()
+  end
+
+  local self = {}
+  function self.show()
+    showModeTooltip()
+  end
+  function self.hide()
+    hideModeTooltip()
+  end
+  return self
+end
+
+
 function text(message)
   return hs.styledtext.ansi(message, {font={name="Fantasque Sans Mono",size=36}, backgroundColor={alpha=1}})
 end
