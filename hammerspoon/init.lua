ee48b4d862b4a09a6b5bd776442765afdd589856
Roman Coedo <romancoedo@gmail.com>
2016-05-24 23:10:36 +0200

    readd atom, goddamit

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -10,6 +10,7 @@ local key = Spacebar.new {
    prefix = "cmd-return",
    bindings = {
       {"i", "intellij", function() window.launchOrFocus("IntelliJ IDEA") end},
+      {"a", "atom", function() window.launchOrFocus("Atom") end},
       {"e", "emacs", function() window.launchOrFocus("Emacs") end},
       {"t", "terminal", function() window.launchOrFocus("iTerm") end},
       {"c", "chrome", function() window.launchOrFocus("Google Chrome") end},
