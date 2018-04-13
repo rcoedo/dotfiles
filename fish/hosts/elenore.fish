698437c43c7a2028074e93cee6af17d667db2aca
Roman Coedo <romancoedo@gmail.com>
2018-04-13 14:19:53 +0200

    Add flutter to hosts/elenore

--- fish/hosts/elenore.fish
+++ fish/hosts/elenore.fish
@@ -1,2 +1,4 @@
+export GHQ_ROOT="$HOME/Workspace/src"
 export ANDROID_HOME=/Volumes/SD/android-sdk
 __add_to_path $ANDROID_HOME/platform-tools
+__add_to_path (ghq root)/github.com/flutter/flutter/bin
