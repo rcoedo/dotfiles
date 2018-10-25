ac6aba7b4352c6ab4a4b181be8ab08cdaa1b74ed
Roman Coedo <romancoedo@gmail.com>
2018-10-25 20:54:53 +0200

    Update fish config

--- fish/hosts/elenore.fish
+++ fish/hosts/elenore.fish
@@ -1,6 +1,6 @@
 export GHQ_ROOT="$HOME/Workspace/src"
 export ANDROID_HOME=/Volumes/SD/android-sdk
 __add_to_path $ANDROID_HOME/platform-tools
-__add_to_path (ghq root)/github.com/flutter/flutter/bin
-bass source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc
-bass source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc
+# __add_to_path (ghq root)/github.com/flutter/flutter/bin
+# bass source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc
+# bass source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc
