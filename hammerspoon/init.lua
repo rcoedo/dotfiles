9d462ffb44289ecbf5e5f78ef019ce96b1da4e51
Roman Coedo <6409659+rcoedo@users.noreply.github.com>
2022-09-26 09:38:10 +0200

    changes to hammerspoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,7 +1,7 @@
+local amphetamine = require "amphetamine"
 local window = require "window"
 local app = require "application"
 local spotify = require "spotify"
-local amphetamine = require "amphetamine"
 local Spacebar = require "modal/spacebar"
 
 -- Reload configuration
@@ -12,24 +12,29 @@ hs.alert("Config loaded")
 local key = Spacebar.new {
    prefix = "cmd-return",
    bindings = {
+      {"cmd-return", "hints", function() hs.hints.windowHints() end },
       {"g", "g", function() app.launchOrFocus("IntelliJ IDEA") end},
       {"i", "intellij", function() app.launchOrFocus("IntelliJ IDEA") end},
       {"u", "android studio", function() app.launchOrFocus("Android Studio") end},
-      {"d", "DBeaver", function() window.focus("DBeaver") end},
       {"o", "code", function() app.launchOrFocus("Visual Studio Code") end},
+      {"r", "webstorm", function() app.launchOrFocus("WebStorm") end},
       {"e", "emacs", function() app.launchOrFocus("Emacs.app") end},
       {"b", "brave", function() app.launchOrFocus("Brave Browser") end},
-      {"f", "fork", function() app.launchOrFocus("Fork") end},
-      {"r", "trello", function() app.launchOrFocus("Trello") end},
+--      {"f", "fork", function() app.launchOrFocus("Fork") end},
+      {"f", "firefox", function() app.launchOrFocus("Firefox") end},
+--      {"r", "trello", function() app.launchOrFocus("Trello") end},
       {"a", "safari", function() app.launchOrFocus("Safari") end},
       {"t", "terminal", function() app.launchOrFocus("iTerm") end},
+--      {"t", "terminal", function() app.launchOrFocus("Alacritty") end},
       {"c", "chrome", function() app.launchOrSwitch("Google Chrome") end},
       {"z", "zoom", function() app.launchOrSwitch("zoom.us") end},
+      {"n", "notion", function() app.launchOrSwitch("Notion") end},
       {"m", "messaging", {
           {"w", "whatsapp", function() app.launchOrSwitch("WhatsApp") end},
           {"d", "discord", function() app.launchOrSwitch("Discord") end},
           {"t", "telegram", function() app.launchOrFocus("Telegram") end},
           {"l", "slack", function() app.launchOrFocus("Slack") end},
+          {"n", "Nheko", function() app.launchOrFocus("Nheko") end},
       }},
       {"s", "spotify mode", {
           {"p", "previous", spotify.previous, {modal = "stay"}},
