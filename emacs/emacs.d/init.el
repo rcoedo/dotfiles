962abf3e1a2eea41e1de42e71a86298ffe1b5e67
Roman Coedo <6409659+rcoedo@users.noreply.github.com>
2022-10-16 11:27:46 +0200

    emacs theme

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -22,7 +22,6 @@
 
 ;; Load helpers
 (load "~/.emacs.d/helpers.el")
-(load "~/.emacs.d/tokyo-theme.el")
 
 ;; The first thing we do is load our environment
 (use-package exec-path-from-shell
@@ -34,14 +33,32 @@
   (exec-path-from-shell-initialize))
 
 ;; Theme
-(use-package gruvbox-theme
-  :init
-  (set-frame-font (font-spec :family "JetBrainsMono NF" :size 13))
-  (set-face-italic 'font-lock-comment-face t)
-  (setq-default line-spacing 3)
-  :config
-  (load-theme 'gruvbox t))
+;; (use-package gruvbox-theme
+;;   :init
+;;   (set-frame-font (font-spec :family "JetBrainsMono NF" :size 13))
+;;   (set-face-italic 'font-lock-comment-face t)
+;;   (setq-default line-spacing 3)
+;;   :config
+;;   (load-theme 'gruvbox t))
 ;;
+
+(use-package doom-themes
+  :ensure t
+  :config
+  (set-frame-font (font-spec :family "JetBrainsMono NF" :size 13))
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-tokyo-night t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  (doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
 
 ;; General
 (use-package general
