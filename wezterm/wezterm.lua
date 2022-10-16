5866899142034258c26693b8cac0b8a5e14d1b02
Roman Coedo <6409659+rcoedo@users.noreply.github.com>
2022-10-16 11:10:43 +0200

    changes to wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require("wezterm")
 local config = {
   --color_scheme = "MonokaiPro (Gogh)",
   color_scheme = "tokyonight",
-  font = wezterm.font('FiraCode NF', { weight = "Regular" }),
+  font = wezterm.font('JetBrains Mono', { weight = "Medium" }),
   font_size = 14,
   window_decorations = "RESIZE",
   hide_tab_bar_if_only_one_tab = true,
