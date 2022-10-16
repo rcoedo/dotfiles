2be6888c5677ed5f43d6fe7b2390f407e8a006d7
Roman Coedo <6409659+rcoedo@users.noreply.github.com>
2022-10-16 10:46:41 +0200

    changes to wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,8 +4,10 @@ local config = {
   --color_scheme = "MonokaiPro (Gogh)",
   color_scheme = "tokyonight",
   font = wezterm.font('FiraCode NF', { weight = "Regular" }),
+  font_size = 14,
   window_decorations = "RESIZE",
   hide_tab_bar_if_only_one_tab = true,
+  send_composed_key_when_right_alt_is_pressed = false,
   --window_padding = {
     --left = 0,
     --right = 0,
