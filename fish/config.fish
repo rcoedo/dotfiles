2c94b7a1dfbfd6abf53e0155e4e4ccc552bddffd
Roman Coedo <6409659+rcoedo@users.noreply.github.com>
2022-10-19 14:15:15 +0200

    update fish

--- fish/config.fish
+++ fish/config.fish
@@ -5,7 +5,11 @@ export GIT_EDITOR="nvim"
 export GREP_OPTIONS="--color=auto"
 export HOMEBREW_NO_ANALYTICS=1
 export HOMEBREW_NO_GITHUB_API=1
-export HOMEBREW_NO_ENV_HINTS=1
+export FZF_DEFAULT_OPTS='
+    --color=fg:#c0caf5,bg:#1a1b26,hl:#bb9af7
+	--color=fg+:#c0caf5,bg+:#292e42,hl+:#7dcfff
+	--color=info:#7aa2f7,prompt:#7dcfff,pointer:#7dcfff
+	--color=marker:#9ece6a,spinner:#9ece6a,header:#9ece6a'
 
 fish_add_path /opt/homebrew/bin
 
@@ -29,6 +33,8 @@ abbr r "ranger"
 abbr ws "webstorm"
 abbr jk "jekyll"
 abbr tree "tree --gitignore"
+abbr ls "exa"
+abbr ll "exa -la"
 
 abbr t "tig"
 abbr ts "tig status"
