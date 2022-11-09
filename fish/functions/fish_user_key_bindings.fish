db3fb92edf71f360ecfdd8d34f860e1be6b3773e
Roman Coedo <6409659+rcoedo@users.noreply.github.com>
2022-11-09 14:54:50 +0100

    add __fuzzy_git_commit

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -2,6 +2,7 @@ function fish_user_key_bindings
   bind \eb 'prevd; commandline -f repaint'
   bind \eu 'cd ..; commandline -f repaint'
   bind \ei '__fuzzy_package_json_script'
+  bind \em '__fuzzy_git_commit'
   bind \cr '__fuzzy_history'
   bind \eo '__fuzzy_lsof'
   bind \ep '__fuzzy_ps'
